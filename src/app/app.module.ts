e99c1d66ba990e4db96bb4dbc148cba2e636a032
Cristian Nass <kristian.nasiri@gmail.com>
2020-08-03 02:38:00 +0200

    created bootstrap

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,17 +1,29 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
 
 import { AppComponent } from './app.component';
 import { PostComponent } from './post/post.component';
 import { HttpClientModule } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
+import { NavbarComponent } from './navbar/navbar.component';
+
+const appRoutes: Routes = [
+  { path: '', component: HomeComponent},
+  { path: 'post', component: PostComponent}
+  ];
+
 @NgModule({
   declarations: [
     AppComponent,
-    PostComponent
+    PostComponent,
+    HomeComponent,
+    NavbarComponent
   ],
   imports: [
     BrowserModule,
-    HttpClientModule
+    HttpClientModule,
+    RouterModule.forRoot(appRoutes)
   ],
   providers: [],
   bootstrap: [AppComponent]
